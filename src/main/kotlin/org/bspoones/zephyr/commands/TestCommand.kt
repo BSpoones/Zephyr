7dbfa8d5a53f71fb7624f3f007226ef07a3544e6
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-03-30 00:10:47 +0000

    rmv: Tests

--- src/main/kotlin/org/bspoones/zephyr/commands/TestCommand.kt
+++ src/main/kotlin/org/bspoones/zephyr/commands/TestCommand.kt
@@ -1,43 +1,18 @@
 package org.bspoones.zephyr.commands
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
-import net.dv8tion.jda.api.interactions.components.selections.EntitySelectMenu
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle
-import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder
 import org.bspoones.zeus.command.Command
+import org.bspoones.zeus.command.annotations.CommandOption
 import org.bspoones.zeus.command.annotations.command.SlashCommand
-import org.bspoones.zeus.component.button.AutoButton
-import org.bspoones.zeus.component.modal.AutoModal
-import org.bspoones.zeus.component.modal.AutoModalInput
-import org.bspoones.zeus.component.select.AutoSelectMenu
-
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.VALUE_PARAMETER)
-annotation class StringChoices(
-    val choices: Array<String>
-)
-
 
 object TestCommand : Command() {
     @SlashCommand("test", "Test command")
     fun onTestCommand(
-        event: SlashCommandInteractionEvent
+        event: SlashCommandInteractionEvent,
+
+        @CommandOption("test","Test choice", isRequired = false)
+        choice: String?
     ) {
-
-        val menu = AutoSelectMenu.EntityOptionBuilder()
-            .customId("test")
-            .placeholder("Test")
-            .selectTarget(EntitySelectMenu.SelectTarget.ROLE)
-            .build()
-
-        val message = MessageCreateBuilder()
-            .setContent("Test")
-            .addActionRow(menu.toEntityMenu())
-            .build()
-
-
-        event.reply(message).queue()
     }
 }
 
