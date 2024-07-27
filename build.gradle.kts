59bc8eef8eddcfdfb4b869996ca89f5caac0ab83
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-07-27 01:21:21 +0100

    feat: Custom logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.spongepowered:configurate-gson:4.1.2")
     implementation("org.slf4j:slf4j-api:1.7.32") // SLF4J dependency
-    implementation("ch.qos.logback:logback-classic:1.2.9") // Logback logging implementation
+    implementation("ch.qos.logback:logback-classic:1.5.6") // Logback logging implementation
     implementation("io.github.cdimascio:java-dotenv:5.2.2") // Dotenv
     implementation(kotlin("reflect"))
     implementation(fileTree("libs"))
